810aa108ef1b47d91ff6a0d9ebd66f6b500bd067
João Neto <joao72neto@gmail.com>
2026-05-11 13:05:55 -0300

    fix(providers): rename to official name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "../providers-temp";
+import Providers from "../providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
