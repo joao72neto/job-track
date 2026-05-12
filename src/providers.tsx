f4480cf8217ef816a1df2314481d57c107d1a413
João Neto <joao72neto@gmail.com>
2026-05-12 11:09:37 -0300

    chore: install tanstack query

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,15 +1,29 @@
+"use client";
+
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "@/src/contexts/auth.context";
 import { ModalProvider } from "@/src/contexts/modal.context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <AuthProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   );
 };
